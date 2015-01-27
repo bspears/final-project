aa65ea5d02cacb2a6238db6c51adf2c9fc877e3f
Ben Spears <bennett.spears@gmail.com>
2015-01-27 09:09:05 -0800

    Add checkbox filter function.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,24 @@ $(document).ready(function() {
 
 
 // City Page------------------------------------
-
-
-
-
-
-
+  $('#restaurant').on('click', function(){
+    $('.restaurant').toggleClass('wanted');
+    $('.location-info').hide();
+    $('.wanted').show('slow');
+  });
+   $('#landmark').on('click', function(){
+    $('.landmark').toggleClass('wanted');
+    $('.location-info').hide();
+    $('.wanted').show('slow');
+  });
+    $('#venue').on('click', function(){
+    $('.venue').toggleClass('wanted');
+    $('.location-info').hide();
+    $('.wanted').show('slow');
+  });
+     $('#outdoors').on('click', function(){
+    $('.outdoors').toggleClass('wanted');
+    $('.location-info').hide();
+    $('.wanted').show('slow');
+  });
 });
