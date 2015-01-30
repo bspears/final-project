0208f9dbd53573eaa0d1f2cda340136151f9cd88
Ben Spears <bennett.spears@gmail.com>
2015-01-29 23:25:28 -0800

    Change listener for each details button to one function.

--- public/js/script.js
+++ public/js/script.js
@@ -12,9 +12,6 @@ $(document).ready(function() {
     $('.showAbout').show('slow');
   });
 
-
-// City Page------------------------------------
-
   $('.locationDetails').hide();
 
   $('#restaurant').on('click', function(){
@@ -55,119 +52,12 @@ $(document).ready(function() {
     $('.location-info').show().removeClass('wanted1 wanted2 wanted3 wanted4');
   });   
 
-
-//Seattle listeners-----------------------------------------
-
-
-//   $('#spaceNeedleExpand').on('click', function() {
-//     $('#spaceNeedleDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//   $('#pikePlaceExpand').on('click', function() {
-//     $('#pikePlaceDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-// //Asheville listeners------------------------------------------
-//   $('#orangePeelExpand').on('click', function() {
-//     $('#orangePeelDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();
-//   });
-
-//   $('#nineMileExpand').on('click', function() {
-//     $('#nineMileDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//   $('#gardensExpand').on('click', function() {
-//     $('#gardensDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-// // Austin listeners ----------------------------------------
-//   $('#bartonSpringsExpand').on('click', function() {
-//     $('#bartonSpringsDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();
-//   });
-
-//   $('#alamoExpand').on('click', function() {
-//     $('#alamoDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//   $('#umlaufExpand').on('click', function() {
-//     $('#umlaufDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-// // New York listeners----------------------------------------
-//   $('#centralParkExpand').on('click', function() {
-//     $('#centralParkDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();
-//   });
-
-//   $('#bronxZooExpand').on('click', function() {
-//     $('#bronxZooDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//   $('#apolloExpand').on('click', function() {
-//     $('#apolloDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-// // Portland listeners-------------------------------------------
-//   $('#crystalBallExpand').on('click', function() {
-//     $('#crystalBallDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();
-//   });
-
-//   $('#irvingStreetExpand').on('click', function() {
-//     $('#irvingStreetDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//   $('#saturdayExpand').on('click', function() {
-//     $('#saturdayDetails').toggleClass('showDetail');
-//     $('.locationDetails').hide();
-//     $('.showDetail').show();      
-//   });
-
-//Comment section JSON
-  // function submitComment(event) {
-  //   event.preventDefault();
-
-  //   var commentSave = {};
-
-  //   commentSave["commentText"] = $("this.textarea").text();
-
-  //   var commentObj = JSON.stringify(commentSave);
-
-  //   localStorage.setItem(insert, commentObj);
-  // }
-
-  // var insert = function() {
-  //   $("#spaceNeedleComments").append("<li>" + commentObj + "</li>");
-  // }
-
-  // $("#spaceNeedlePost").submit(submitComment);
-
-// Favorite list----------------------------------
-
+ $('.expand').on('click', function() {
+    var $this = $(this);
+    $(this).next().toggleClass('showDetail');
+    $('.locationDetails').hide();
+    $('.showDetail').show();
+  });
 });
 
 function Location(city, name) {
@@ -199,39 +89,30 @@ var data = {
 
 var jsonFavs;
 
-
-  // var storedFavs = JSON.parse(data['favList']);
-
-
+var setFavs = (function () {
+  var savedPlaces = [];
+  var parse = JSON.parse(localStorage['storedFavs']);
+  $.each(parse.favList, function() {
+    savedPlaces.push ('<li>'+ this.name +'<li>')
+  });
+  $('#favItems').html(savedPlaces.join(' '));
+}());
 
 $('.favButton').on('click', function() {
   data['favList'].push (locations[this.id]);
   localStorage['storedFavs'] = JSON.stringify(data);
-  jsonFavs = JSON.parse(localStorage['storedFavs']);
-  $.each(jsonFavs.favList, function() { 
-    $('#favItems').append('<li>' +this.name+ '</li>');
-  })
+  var savedPlaces = [];
+  var parse = JSON.parse(localStorage['storedFavs']);
+  $.each(parse.favList, function() {
+    savedPlaces.push ('<li>'+ this.name +'<li>')
+  });
+  $('#favItems').html(savedPlaces.join(' '));
 });
 
 
 
-// $('.favButton').on('click', function() {
-//     var place;
-//     $('#favItems').append('<li>' + event.currenttarget.value + '</li>');
-//   });
 
-
-//JSON-------------------------------------------
-
-
-
-
-  $('.expand').on('click', function() {
-    var $this = $(this);
-    $this.toggleClass('showDetail');
-    $('.locationDetails').hide();
-    $('.showDetail').show();
-  });
+ 
 
 
 
